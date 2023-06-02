4884d1faa922706836f9f6ac3ab9db8bfc459a3b
simonaditia <smnsitumeang@gmail.com>
2023-06-02 19:25:17 +0700

    Add feature_product

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(id, name, desc, type) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+    }
+}
+
+module.exports = Student;
